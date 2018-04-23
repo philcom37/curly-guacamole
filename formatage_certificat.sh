6b4e48ca6b7cfa3b6c1018e846677874a809786a
philcom37 <philcom37@free.fr>
2018-04-23 19:31:53 +0200

    modification du script pour prendre en compte les nouveaux repertoires créés

--- formatage_certificat.sh
+++ formatage_certificat.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Script de formatage du certificat avec ajout de commentaires en début de PEM pour faciliter la lecture du certificat
 # ====================================================================================================================
+repertoire_test=certificat_test
+repertoire_resultat=resultat
+cd $repertoire_test
 for user in `ls *.pem`
 do
 # formatage du certificat avec ajout de commentaires en début de PEM pour faciliter la lecture du certificat
-openssl x509 -in $user -text -noout | egrep -i "issuer:|subject:" | sed 's/^ */#/' > $user-BUREAU.pem;
+openssl x509 -in $user -text -noout | egrep -i "issuer:|subject:" | sed 's/^ */#/' > ../$repertoire_resultat/$user-RESUME.pem;
 done
